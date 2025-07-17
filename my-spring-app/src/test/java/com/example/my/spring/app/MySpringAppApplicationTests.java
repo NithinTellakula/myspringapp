0ffd4d450db753f912059d7873c730a2e83da3f2
NithinTellakula <nithintellakula2003@gmail.com>
2025-07-17 15:22:03 +0530

    this is initial push

---
+++ my-spring-app/src/test/java/com/example/my/spring/app/MySpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my.spring.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
